db74df3cb28a097c7e5311faa61538c14af95f6a
JorgeMGuimaraes <jmguimaraes@id.uff.br>
2024-04-25 17:39:31 -0300

    fix: split home from widgets

---
+++ aula_inputs/lib/widgets/inputs_home/inputs_home.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class InputsHome extends StatefulWidget {
+  const InputsHome({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<InputsHome> createState() => _InputsHomeState();
+}
+
+class _InputsHomeState extends State<InputsHome> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            TextField(
+              obscureText: true,
+              onChanged: (value) => print("Value $value")
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
